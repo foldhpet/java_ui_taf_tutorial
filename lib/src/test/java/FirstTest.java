79709340cf63cb1b88353044a60a6d514733136c
Péter Földházi Jr. <10651180+foldhpet@users.noreply.github.com>
2024-09-23 11:42:30 -0700

    Implement OpenHomePage test

--- lib/src/test/java/FirstTest.java
+++ lib/src/test/java/FirstTest.java
@@ -1,3 +1,22 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FirstTest {
     
+    @Test
+    public void OpenHomePage() {
+        // Setup WebDriver to communicate with FireFox
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBinary("C:/Program Files/Mozilla Firefox/firefox.exe");
+        WebDriver driver = new FirefoxDriver(options);
+
+        // Your test case starts here
+        driver.get("https://www.peterfoldhazi.com");
+        driver.quit();
+    }
 }
