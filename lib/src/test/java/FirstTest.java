e1d2ee28e917754cea014ef32566788c285a5cc9
Péter Földházi Jr. <10651180+foldhpet@users.noreply.github.com>
2024-09-24 12:55:29 -0700

    Added more steps

--- lib/src/test/java/FirstTest.java
+++ lib/src/test/java/FirstTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -27,5 +28,26 @@ public class FirstTest {
     @Test
     public void OpenHomePage() {
         driver.get("https://www.peterfoldhazi.com");
+    }
+
+    @Test
+    public void OpenTheFlowModelPatternArticle() {
+        //step 1: Open home page
+        driver.get("https://www.peterfoldhazi.com");
+
+        //step 2: Open About page through the menu
+        driver.findElement(By.id("comp-kd46oy0r1label")).click();
+
+        //step 3: Open Conferences page through the menu
+        driver.findElement(By.id("comp-kd46oy0r2label")).click();
+        
+        //step 4: Open Articles page through the menu
+        driver.findElement(By.id("comp-kd46oy0r3label")).click();
+        
+        //step 5: Open Blog page through the menu
+        driver.findElement(By.id("comp-kd46oy0r4label")).click();
+        
+        //step 6: Open Contact page through the menu
+        driver.findElement(By.id("comp-kd46oy0r5label")).click();
     }
 }
